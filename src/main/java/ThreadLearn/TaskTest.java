aa829d086cb907575dec090fca77c5e8e34effe0
yiyan1992 <liang-shan@outlook.com>
2019-01-13 23:06:09 +0800

    CONSTANT

---
+++ src/main/java/ThreadLearn/TaskTest.java
@@ -0,0 +1,18 @@
+package ThreadLearn;
+
+import java.util.concurrent.*;
+
+/**
+ * @author admin
+ */
+public class TaskTest {
+
+    public static void main(String[] args) {
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+
+
+    }
+
+}
