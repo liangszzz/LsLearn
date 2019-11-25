0a04621cf7b1b54b7c1c96daa24fc9f7b8c1e7f9
liang <liang-shan@outlook.com>
2019-11-25 16:19:20 +0800

    transaction message

---
+++ src/main/java/rocketmq/transaction/TransactionConsumer.java
@@ -0,0 +1,27 @@
+package rocketmq.transaction;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class TransactionConsumer {
+    public static void main(String[] args) throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_group");
+        consumer.setNamesrvAddr("localhost:9876");
+        consumer.subscribe("base_topic", "*");
+        consumer.registerMessageListener(new MessageListenerOrderly() {
+            @Override
+            public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msgs, ConsumeOrderlyContext context) {
+                msgs.forEach(e -> {
+                    System.out.println(Thread.currentThread().getId() + new String(e.getBody()));
+                });
+                return ConsumeOrderlyStatus.SUCCESS;
+            }
+        });
+        consumer.start();
+    }
+}
